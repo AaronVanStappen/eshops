999ff99b6658802813e0067aeb90e2b75c885206
Aaron <aaronvanstappen@gmail.com>
2017-09-27 21:37:28 +0200

    GUI - EshopSwingApp aangemaakt, methodes en toepassingen aan het uitzoeken en uittesten

--- src/main/java/be/vdab/ui/EshopSwingApp.java
+++ src/main/java/be/vdab/ui/EshopSwingApp.java
@@ -1,4 +1,37 @@
 package be.vdab.ui;
 
-public class EshopSwingApp {
+import javax.swing.*;
+import java.awt.*;
+
+public class EshopSwingApp extends JFrame {
+    private JTable tblProducts = new JTable();
+    private JScrollPane spProducts = new JScrollPane(tblProducts);
+
+    private EshopSwingApp() {
+        initComponents();
+        layoutComponents();
+        initListeners();
+    }
+
+    private void initComponents() {
+        setTitle("E-shop App");
+        setSize(1200, 700);
+        setLocation(200, 200);
+        setVisible(true);
+    }
+
+    private void layoutComponents() {
+        JPanel middlePanel = new JPanel();
+        middlePanel.add(spProducts);
+        add(middlePanel, BorderLayout.CENTER);
+    }
+
+    private void initListeners() {
+
+    }
+
+    public static void main(String[] args) {
+        new EshopSwingApp();
+    }
+
 }
